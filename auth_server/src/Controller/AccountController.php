a1aa19aee36fa48a588d2a38f75c54ab8d95ace6
Luis Sanchez Castellanos <luis.sanchez@mosaicsmartdata.com>
2023-07-31 16:14:44 +0200

    Backend cleanup and using json

--- auth_server/src/Controller/AccountController.php
+++ auth_server/src/Controller/AccountController.php
@@ -9,47 +9,41 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountController extends AbstractController
 {
-    #[Route('/api/account', name: 'account_current', methods: ["GET"])]
-    public function current(): JsonResponse
-    {
-        return $this->json([
-            'error' => 'To do'
-        ], 500);
-    }
-
     #[Route('/api/account', name: 'account_register', methods: ["POST"])]
-    public function register(Request $request,EntityManagerInterface $entityManager): JsonResponse
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
+        $data = $request->toArray();
         $account = new Account();
-        $account->setName($request->request->get('name'));
-        $account->setEmail($request->request->get('email'));
-        $account->setAddressNumber($request->request->get('address_number'));
-        $account->setAddress($request->request->get('address'));
-        $account->setCity($request->request->get('city'));
-        $account->setPostcode($request->request->get('postcode'));
-        $account->setPassword($request->request->get('password'));
+        $account->setName($data['name']);
+        $account->setEmail($data['email']);
+        $account->setAddressNumber($data['address_number']);
+        $account->setAddress($data['address']);
+        $account->setCity($data['city']);
+        $account->setPostcode($data['postcode']);
         $account->setRoles(['USER_ACCOUNT']);
+
+        $hashedPassword = $passwordHasher->hashPassword($account, $data['password']);
+        $account->setPassword($hashedPassword);
 
         $entityManager->persist($account);
         try {
             $flush = $entityManager->flush();
-            print "FLUSH";
-            var_dump($flush);
             return $this->json([
                 'payload' => [
-                    'name' => $request->request->get('name'),
-                    'email' => $request->request->get('email'),
-                    'address_number' => $request->request->get('address_number'),
-                    'address' => $request->request->get('address'),
-                    'city' => $request->request->get('city'),
-                    'postcode' => $request->request->get('postcode')
+                    'name' => $data['name'],
+                    'email' => $data['email'],
+                    'address_number' => $data['address_number'],
+                    'address' => $data['address'],
+                    'city' => $data['city'],
+                    'postcode' => $data['postcode']
                 ]
             ]);
         } catch (UniqueConstraintViolationException $e) {
-            $email = $request->request->get('email');
+            $email = $data['email'];
             return $this->json([
                 'error' => "Account with email $email already exists."
             ], 409);
--- auth_server/tests/Controller/AccountControllerTest.php
+++ auth_server/tests/Controller/AccountControllerTest.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class AccountControllerTest extends WebTestCase
 {
     private static $email;
+    private static $hashedPassword;
 
     public static function setUpBeforeClass(): void
     {
@@ -17,9 +18,9 @@ class AccountControllerTest extends WebTestCase
 
     public function testRegister(): void
     {
-        $client = static::createClient();
-        $client->request('POST', '/api/account',
-            [
+        $client = static::createClient([], ['Content-Type' => 'application/json']);
+        $client->request('POST', '/api/account', [],[],[],
+            json_encode([
                 'name'=>'Luis Sanchez',
                 'email' => self::$email,
                 'address_number' => '17',
@@ -27,7 +28,7 @@ class AccountControllerTest extends WebTestCase
                 'city' => 'London',
                 'postcode' => 'SE16',
                 'password' => 'a_text-plain_password'
-            ]
+            ])
         );
         $response = $client->getResponse();
         $this->assertResponseIsSuccessful();
@@ -46,8 +47,8 @@ class AccountControllerTest extends WebTestCase
     public function testRegisterExisting(): void
     {
         $client = static::createClient();
-        $client->request('POST', '/api/account',
-            [
+        $client->request('POST', '/api/account', [],[],[],
+            json_encode([
                 'name'=>'Luis Sanchez',
                 'email' => self::$email,
                 'address_number' => '17',
@@ -55,33 +56,8 @@ class AccountControllerTest extends WebTestCase
                 'city' => 'London',
                 'postcode' => 'SE16',
                 'password' => 'a_text-plain_password'
-            ]
+            ])
         );
         $this->assertResponseStatusCodeSame(409);
-    }
-
-    public function testCurrent(): void
-    {
-        $client = static::createClient();
-        $client->request('GET', '/api/login_check',
-            [
-                'email' => self::$email,
-                'password' => 'a_text-plain_password'
-            ]);
-        $response = $client->getResponse();
-
-        $client->request('GET', '/api/account');
-        $response = $client->getResponse();
-        $this->assertResponseIsSuccessful();
-        $data = json_decode($response->getContent(), true);
-        $this->assertArrayHasKey('payload', $data);
-        $this->assertObjectEquals($data['payload'], [
-            'name'=>'Luis Sanchez',
-            'email' => self::$email,
-            'address_number' => '17',
-            'address' => 'Street Fighter',
-            'city' => 'London',
-            'postcode' => 'SE16'
-        ]);
     }
 }
